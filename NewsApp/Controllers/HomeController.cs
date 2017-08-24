c84037738705acfb01c6e802bc92f528d3cd1146
galstyankaren <karen_galstyan@edu.aua.am>
2017-08-24 12:08:27 +0400

    Connected to the news API endpoint.Created the Master view for article listing

--- NewsApp/Controllers/HomeController.cs
+++ NewsApp/Controllers/HomeController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NewsApp.Models;
+using Newtonsoft.Json;
 
 namespace NewsApp.Controllers
 {
     public class HomeController : Controller
     {
+        //Endpoint to the NEws API
+        private static string ENDPOINT = "https://www.sicher-im-netz.de/siba-app/siba/list/0";
+
+        /// <summary>
+        /// Home Page (Master view) of the app. Returns a list of articles from the NEWS API.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
-            return View();
+
+            Rootobject root = new Rootobject();
+
+            using (var webClient = new System.Net.WebClient())
+            {
+                var json = webClient.DownloadString(ENDPOINT);
+
+                //Not directly deserialized to Rootobject as the JSON is in the [{}] form.
+                root.articles = JsonConvert.DeserializeObject<List<JsonArticle>>(json);
+            }
+
+
+            return View(root.articles);
         }
+
+        /// <summary>
+        /// Details view
+        /// </summary>
+        /// <param name="_id"></param>
+        /// <returns></returns>
         public ActionResult Details(int _id)
         {
             return View();
